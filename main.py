8a553ef81a2af033859c7ec0ae430e371e411c1d
premiumytmk234-coder <premiumytmk234@gmail.com>
2026-04-03 19:55:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 5840594311 # Replace with the actual owner's user ID
+OWNER_ID = 8295147093 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [5840594311]
+SUDO_USERS = [8295147093]
 
 # ✅ Multiple AUTH CHANNELS allowed
 AUTH_CHANNELS = [-1002605113558,-1002663510614]  # Add more channel IDs here
